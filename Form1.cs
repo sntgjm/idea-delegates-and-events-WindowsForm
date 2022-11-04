71c4fdfb5cf64777997de5b2400d8d60c3b9694a
sntgjm <jm2001santiago@gmail.com>
2022-11-04 18:06:27 +0800

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AccountRegistration
+{
+    public delegate long DelegateNumber(long number);
+    public delegate string DelegateText(string text);
+    
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox7_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2();
+            StudentInfoClass.Program = comboBox1.Text.ToString();
+            StudentInfoClass.LastName = textBox2.Text.ToString();
+            StudentInfoClass.FirstName = textBox3.Text.ToString();
+            StudentInfoClass.MiddleName = textBox4.Text.ToString();
+            StudentInfoClass.Address = textBox7.Text.ToString();
+            StudentInfoClass.Age = Convert.ToInt64(textBox5.Text);
+            StudentInfoClass.ContactNo = Convert.ToInt64(textBox6.Text);
+            StudentInfoClass.StudentNo = Convert.ToInt64(textBox1.Text);
+
+            if (form2.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Clear();
+                textBox2.Clear();
+                textBox3.Clear();
+                textBox4.Clear();
+                textBox5.Clear();
+                textBox6.Clear();
+                textBox7.Clear();
+                comboBox1.Text = "";
+            }
+        }
+    }
+    class StudentInfoClass
+    {
+        public static string FirstName = "";
+        public static string LastName = "";
+        public static string MiddleName = "";
+        public static string Address = "";
+        public static string Program = "";
+        public static long Age = 0;
+        public static long ContactNo = 0;
+        public static long StudentNo = 0;
+
+        public static string GetFirstName(string FirstName)
+        {
+            return FirstName;
+        }
+        public static string GetLastName(string LastName)
+        {
+            return LastName;
+        }
+        public static string GetMiddleName(string MiddleName)
+        {
+            return MiddleName;
+        }
+        public static string GetAddress(string Address)
+        {
+            return Address;
+        }
+        public static string GetProgram(string Program)
+        {
+            return Program;
+        }
+        public static long GetAge(long Age)
+        {
+            return Age;
+        }
+        public static long GetContactNo(long ContactNo)
+        {
+            return ContactNo;
+        }
+        public static long GetStudentNo(long StudentNo)
+        {
+            return StudentNo;
+        }
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AccountRegistration
+{
+    public partial class Form2 : Form
+    {
+        private DelegateText DelProgram, DelLastName, DelFirstName, DelMiddleName, DelAddress;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.OK;
+            this.Close();
+        }
+        private DelegateNumber DelNumAge, DelNumContactNo, DelStudNo;
+        public Form2()
+        {
+            InitializeComponent();
+            DelFirstName = new DelegateText(StudentInfoClass.GetFirstName);
+            DelLastName = new DelegateText(StudentInfoClass.GetLastName);
+            DelMiddleName = new DelegateText(StudentInfoClass.GetMiddleName);
+            DelProgram = new DelegateText(StudentInfoClass.GetProgram);
+            DelAddress = new DelegateText(StudentInfoClass.GetAddress);
+            DelNumAge = new DelegateNumber(StudentInfoClass.GetAge);
+            DelNumContactNo = new DelegateNumber(StudentInfoClass.GetContactNo);
+            DelStudNo = new DelegateNumber(StudentInfoClass.GetStudentNo);
+            label1.Text = Convert.ToString(DelStudNo(StudentInfoClass.StudentNo));
+            label6.Text = Convert.ToString(DelNumAge(StudentInfoClass.Age));
+            label7.Text = Convert.ToString(DelNumContactNo(StudentInfoClass.ContactNo));
+            label3.Text = DelLastName(StudentInfoClass.LastName);
+            label4.Text = DelFirstName(StudentInfoClass.FirstName);
+            label5.Text = DelMiddleName(StudentInfoClass.MiddleName);
+            label8.Text = DelAddress(StudentInfoClass.Address);
+            label2.Text = DelProgram(StudentInfoClass.Program);
+
+        }
+
+        private void Form2_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            this.DialogResult = DialogResult.OK;
+        }
+        private void Form2_Load(object sender, EventArgs e)
+        {
+        }
+
+    }
+}
