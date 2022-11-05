2116c5fbce5153597cae3bab7a9004ac8217ae3b
Jm Santiago <116279303+sntgjm@users.noreply.github.com>
2022-11-05 22:01:13 +0800

    updated

--- Form1.cs
+++ Form1.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+//sntgjm
 namespace AccountRegistration
 {
     public delegate long DelegateNumber(long number);
